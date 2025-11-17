17ceb9cf255d6c84cdcb7ed9e604b222ebed2fac
Kamo <ballaholic.1986@gmail.com>
2025-11-17 13:58:53 +0900

    automation-update

--- my-playwright/playwright.config.ts
+++ my-playwright/playwright.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
+
 console.log(":めくったページ: config 読み込まれたよ！");
+
 export default defineConfig({
-  testDir: 'tests',  // テストファイルが存在するディレクトリ
-  reporter: [['html', { outputFolder: 'playwright-report' }]],
-  timeout: 30000,    // テストのタイムアウト（ミリ秒）
+  testDir: 'tests',
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      }
+    },
+  ],
+
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  timeout: 30000,
+
   use: {
     baseURL: 'https://dev.d1x2fefh4glbva.amplifyapp.com/auth/sign-in',
     storageState: 'storage/auth.json',
-    headless: false, // ヘッドレスモード（trueにするとブラウザが表示されない）
-    screenshot: 'only-on-failure',  // 失敗した場合のみスクリーンショットを撮る
-    video: 'on',     // テスト実行中の動画を記録
+    headless: false,
+    screenshot: 'only-on-failure',
+    video: 'on',
   },
 });

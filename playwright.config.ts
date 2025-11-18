82f7b69a7efe1049a051f5ed77f8fea4ae038c91
Keiichi Sakamoto <keiichi.sakamoto@medimo.ai>
2025-11-19 00:45:06 +0900

    Add Playwright config and test setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   use: {
+    headless: true,  // ← ここを追加
     storageState: path.resolve(__dirname, 'storage/auth.json'),
     // ここに共通のブラウザ設定を追加できます
   },
